f576e19ffbdedcc62551d8cd2700e80ee69eeb5e
JulianSolo1984 <juliansolowork@gmail.com>
2025-07-14 14:48:37 -0300

    Projeto FrontEnd Finalizado

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { CartProvider } from './contexts/cartContext';
-import App from './app';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
